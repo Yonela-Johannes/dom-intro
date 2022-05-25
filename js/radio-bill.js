86cf4279bd8c8b5a0a8a41dae6f7680d0c294061
Yonela-Johannes <johannesyonela@gmail.com>
2022-05-25 11:18:47 +0200

    radio bill with handlebars

--- js/radio-bill.js
+++ js/radio-bill.js
@@ -1,23 +1,31 @@
 import { RadioBillFactory } from "./text-bill-factory.js"
 const radioBillFactory = RadioBillFactory()
 const billItemTypeRadio = document.querySelectorAll('.billItemTypeRadio')
-const billColor = document.querySelector('.totalTwoColor')
-let displayCallTotalTwo = document.querySelector('.callTotalTwo'), displaySmsTotalTwo = document.querySelector('.smsTotalTwo')
 const radioBillAddBtn = document.querySelector('.radioBillAddBtn')
-let totalAmount = 0, totalCallTwo = 0, currentCall = 2.75, totalSmsTwo = 0, currentSms = 0.75
-let displayTotalTwo = document.querySelector('.totalTwo')
-displayCallTotalTwo.innerHTML = radioBillFactory.getCallTotal().toFixed(2)
-displaySmsTotalTwo.innerHTML = radioBillFactory.getSmsTotal().toFixed(2)
-displayTotalTwo.innerHTML = radioBillFactory.getTotal().toFixed(2)
+const radioBillTemplate = () => {
+    const templateSourceTwo = document.querySelector(".radioTemplate").innerHTML
+    const radioTemplate = Handlebars.compile(templateSourceTwo)
+    const displayElem = document.querySelector(".radioData")
+    const displayRadioData = radioTemplate({
+        callCost: radioBillFactory.getCallTotal().toFixed(2),
+        smsCost: radioBillFactory.getSmsTotal().toFixed(2),
+        radioTotalCost: radioBillFactory.getTotal().toFixed(2)
+    })
+    displayElem.innerHTML = displayRadioData
+}
+radioBillTemplate()
+const radioDisplayColor = () => {
+    const totalTwo = document.querySelector(".totalTwo");
+    totalTwo.classList.remove("warning");
+    totalTwo.classList.remove("danger");
+    totalTwo.classList.add(radioBillFactory.classes())
+}
 radioBillAddBtn.addEventListener('click', () => {
     for (let i = 0; i < billItemTypeRadio.length; i++) {
         billItemTypeRadio[i].value == 'call' & billItemTypeRadio[i].checked && radioBillFactory.makeCall()
         billItemTypeRadio[i].value == 'sms' & billItemTypeRadio[i].checked && radioBillFactory.sendSms()
     }
     radioBillFactory.setTotal()
-    displayCallTotalTwo.innerHTML = radioBillFactory.getCallTotal().toFixed(2)
-    displaySmsTotalTwo.innerHTML = radioBillFactory.getSmsTotal().toFixed(2)
-    displayTotalTwo.innerHTML = radioBillFactory.getTotal().toFixed(2)
-
-    billColor.classList.add(radioBillFactory.classes())
+    radioBillTemplate()
+    radioDisplayColor()
 })
